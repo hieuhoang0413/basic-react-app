539c1aa63542fd3896fa1ab422650da37129a27b
hieuhoang0413 <nguyenhieu2072004@gmail.com>
2024-06-12 00:06:38 +0700

    first commit

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,10 +1,39 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUsers: true
+    }
+
     render = () => {
+        const { listUsers } = this.props;
+
+        const handleShowHide = () => {
+            this.setState({
+                isShowListUsers: !this.state.isShowListUsers
+            })
+        }
+
         return (
             <div>
-                <h2>My name is {this.props.name}</h2>
+                {this.state.isShowListUsers &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={user.age > 15 ? "green" : "red"}>
+                                    <ul>
+                                        <li>Name: {user.name}</li>
+                                        <li>Age: {user.age}</li>
+                                    </ul>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+                <button onClick={handleShowHide}>
+                    {this.state.isShowListUsers === true ? "Hide list users" : "Show list users"}
+                </button>
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,11 +3,31 @@ import UsersInfo from "./UsersInfo";
 import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            {
+                id: 1,
+                name: "Ngọc Hiểu",
+                age: 20
+            },
+            {
+                id: 2,
+                name: "Hiểu Hoàng",
+                age: 10
+            },
+            {
+                id: 3,
+                name: "Khánh Ngọc",
+                age: 16
+            }
+        ]
+    }
+
     render() {
         return (
             <div>
                 <UsersInfo />
-                <DisplayInfo name="Ngọc Hiểu" />
+                <DisplayInfo listUsers={this.state.listUsers} />
             </div>
         );
     }
